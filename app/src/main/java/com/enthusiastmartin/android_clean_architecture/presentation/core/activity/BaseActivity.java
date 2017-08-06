97d79406aca2e7fe7df1f01f2eedc622ff4e410b
Martin <martin.hloska@gmail.com>
2017-08-06 10:56:17 +0200

    initial core package

---
+++ app/src/main/java/com/enthusiastmartin/android_clean_architecture/presentation/core/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.enthusiastmartin.android_clean_architecture.presentation.core.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by martin on 8/6/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/enthusiastmartin/android_clean_architecture/presentation/module/main/activity/MainActivity.java
+++ app/src/main/java/com/enthusiastmartin/android_clean_architecture/presentation/module/main/activity/MainActivity.java
@@ -1,8 +1,10 @@
 package com.enthusiastmartin.android_clean_architecture.presentation.module.main.activity;
 
+import com.enthusiastmartin.android_clean_architecture.presentation.core.activity.BaseActivity;
+
 /**
  * Created by martin on 8/6/17.
  */
 
-public class MainActivity {
+public class MainActivity extends BaseActivity {
 }
