c3b212d320f37f75a9bc7e0b5775e5b5892f30a2
Martin <martin.hloska@gmail.com>
2017-08-07 11:13:35 +0200

    added use case base class

---
+++ app/src/main/java/com/enthusiastmartin/android_clean_architecture/data/repository/PlaceholderDataRepository.java
@@ -0,0 +1,14 @@
+package com.enthusiastmartin.android_clean_architecture.data.repository;
+
+import com.enthusiastmartin.android_clean_architecture.domain.repository.PlaceholderRepository;
+
+/**
+ * Created by martin on 8/7/17.
+ */
+
+public class PlaceholderDataRepository  implements PlaceholderRepository{
+  /**
+   *  Just Placeholder to show how and where to implement the repository interface
+   *
+   */
+}
